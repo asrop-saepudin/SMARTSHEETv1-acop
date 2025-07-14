67f0c85ed6be49ac6f6c83552451637debb64511
asrop-saepudin <pkm47@teknokrat.ac.id>
2025-07-14 16:24:58 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id('14EXDQFZ3w3m20W1w3EBl0MGCO9hxNz63ZWhC4tQewYY')
+config_artikel.sheet_id('1ZITFsn1hRhdbfXiBG9zb1kC7uRx0iVlPWjIuUtSeTI0')
 config_artikel.gid('710523043')
 
 
